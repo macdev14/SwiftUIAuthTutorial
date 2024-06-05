224cff7464b27e15440383c3e07b9e5b10e9a8d2
Lauro <lauropimentel12@gmail.com>
2024-06-05 18:57:23 -0300

    Initial Commit

---
+++ SwiftUIAuthTutorial/SwiftUIAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthTutorialApp.swift
+//  SwiftUIAuthTutorial
+//
+//  Created by Lauro Pimentel on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
